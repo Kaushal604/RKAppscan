f42a94aaff7274b7cd66fc9a8d9b69d3ba7636ce
Kaushal604 <67890588+Kaushal604@users.noreply.github.com>
2022-11-07 11:41:05 +0530

    Update postjob.sh

--- postjob.sh
+++ postjob.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl http://10.83.120.21:4444/v2/issues/pushjobs -X POST -H "Content-Type:application/json" -H "Accept: application/json" -d @data.json | jq '.'
+curl  http://`(hostname -I | awk '{print $1}')`:4444/v2/issues/pushjobs -X POST -H "Content-Type:application/json" -H "Accept: application/json" -d @data.json | jq '.'
